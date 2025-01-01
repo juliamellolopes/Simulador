ad949e0a7981fc689eefc4fd6c4f4b64cc7ccda8
João Marcos <75141271+joaomarcoshess@users.noreply.github.com>
2025-01-01 15:40:42 -0300

    Update ula.cpp

--- src/ula.cpp
+++ src/ula.cpp
@@ -1,17 +1,11 @@
 #include "ula.h"
 #include <iostream>
 
-int ULA::executar(int var1, int var2, char operador) {
-    switch (operador) {
-    case '+': return var1 + var2;
-    case '-': return var1 - var2;
-    case '*': return var1 * var2;
-    case '/': 
-        if (var2 != 0) return var1 / var2;
-        std::cerr << "Erro: Divisão por zero!" << std::endl;
-        exit(EXIT_FAILURE);
-    default:
-        std::cerr << "Operação inválida!" << std::endl;
-        exit(EXIT_FAILURE);
-    }
+int ULA::executarOperacao(int operando1, int operando2, string operacao) {
+    if (operacao == "ADD") return operando1 + operando2;
+    if (operacao == "SUB") return operando1 - operando2;
+    if (operacao == "AND") return operando1 & operando2;
+    if (operacao == "OR") return operando1 | operando2;
+    cerr << "Operação inválida: " << operacao << endl;
+    exit(EXIT_FAILURE);
 }

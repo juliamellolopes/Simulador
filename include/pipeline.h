e97c95d242e1fa6447c74438736a6c3741fe6af9
João Marcos <75141271+joaomarcoshess@users.noreply.github.com>
2025-01-01 15:21:15 -0300

    Update pipeline.h

--- include/pipeline.h
+++ include/pipeline.h
@@ -3,24 +3,26 @@
 
 #include "memory.h"
 #include "cpu.h"
-
-#define DBG(x) std::cerr << #x << " = " << (x) <<std::endl;
+#include <string>
+#include <vector>
+#include <sstream>
 
 class Pipeline {
-public:
-    Pipeline() {}                                             // Construtor padrão.
-    Pipeline(MemoryRAM &memory, CPU &cpu);                    // Inicializa o pipeline com referências para a memória e CPU.
-    void InstructionFetch();                                  // Busca a próxima instrução na memória.
-    void InstructionDecode();                                 // Decodifica a instrução buscada, identificando o opcode e registradores.
-    void Execute(string code);                                // Executa a instrução baseada no código passado.
-    vector<string>tokenizar(string &instrucao);               // Separa uma instrução em tokens, retornando um vetor de strings.
-    void loop();                                              // Controla o ciclo de execução das instruções, monitorando a execução completa do conjunto de instruções.
-
 private:
-    string _instrucaoAtual;                                   // Armazena a instrução que está sendo processada.
-    string _opcode;                                           // Guarda o opcode da instrução atual.
-    MemoryRAM _memoryRAM;                                     // Referência à memória RAM.
-    CPU _cpu;                                                 // Referência à CPU.
+    MemoryRAM &_memoryRAM;
+    CPU &_cpu;
+    std::string _instrucaoAtual;
+    std::string _opcode;
+
+    int obterIndiceRegistrador(const std::string &reg);
+
+public:
+    Pipeline(MemoryRAM &memory, CPU &cpu);
+    void loop();
+    void InstructionFetch();
+    void InstructionDecode();
+    void Execute(std::string code);
+    std::vector<std::string> tokenizar(std::string &instrucao);
 };
 
 #endif
